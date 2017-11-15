8123571a3571d1c0b780362340e561dd2a995cdb
ohtaehyun <tmzkflsk118@naver.com>
2017-11-15 10:43:22 +0900

    birthday

--- button1.py
+++ button1.py
@@ -4,7 +4,9 @@ window =Tk()
 
 b1 = Button(window, text="오태현")
 b2 = Button(window, text="60142304")
+b3 = Button(window,text="19950709")
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
+b3.grid(row=1,column=0)
 
 window.mainloop()
